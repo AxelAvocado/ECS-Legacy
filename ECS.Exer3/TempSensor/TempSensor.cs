a8498aed1fedb0bdc22ef557f1b10f46f1145149
nne_developer <developer@buildingnordic.com>
2019-09-11 13:54:45 +0200

    Fake classes and interfaces added

---
+++ ECS.Exer3/TempSensor/TempSensor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Exer3.TempSensor
+{
+    class TempSensor : ITempSensor
+    {
+    }
+}
