82677688c60ee90a31bd40a42519065b82eff562
nne_developer <developer@buildingnordic.com>
2019-09-12 13:55:11 +0200

    .

--- ECS.Exer3.Test.Unit/ECSExer3Test.cs
+++ ECS.Exer3.Test.Unit/ECSExer3Test.cs
@@ -4,19 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ECS.Exer3.ECS;
 
 namespace ECS.Exer3.Test.Unit
 {
     [TestFixture]
     public class ECSExer3Test
     {
-        private Cal.Calculat uut;
-
+        private ECS.Exer3.ECS.ECS _uut;
+        
 
         [SetUp]
         public void Setup()
         {
-            _uut = new Cal.Calculator();
+            _uut = new ECS.Exer3.ECS.ECS();
         }
     }
 }
